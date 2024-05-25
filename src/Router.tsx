4bcbfea91d2453bfb231178713047e783bf846cd
OKeattisak.A <OKeattisak.A@gmail.com>
2024-05-25 20:45:26 +0700

    Init

---
+++ src/Router.tsx
@@ -0,0 +1,7 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([]);
+
+export function Router() {
+  return <RouterProvider router={router} />;
+}
