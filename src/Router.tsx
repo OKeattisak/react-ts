e0819b30119f4d82d1d9caf2f50123de03c9f634
OKeattisak.A <OKeattisak.A@gmail.com>
2024-05-25 21:02:19 +0700

    Uodate .gitignore

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,26 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./Layout";
+import Dashboard from "./components/Dashboard";
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Navigate replace to="/dashboard" />,
+      },
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      },
+    ],
+  },
+]);
 
 export function Router() {
   return <RouterProvider router={router} />;
